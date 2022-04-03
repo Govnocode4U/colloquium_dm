090f03679b9c0ea6ed553aea506d16b18fff270c
ilya-shlom <ilyashlom32@gmail.com>
2022-04-04 00:06:46 +0300

    Added Z-9, unfinished (will be finished on 4.04)

---
+++ DIV_ZZ_Z.py
@@ -0,0 +1,14 @@
+def ABS_Z_N(b, n, A):
+    pass
+
+
+def POZ_Z_D(b, n, A):
+    pass
+
+
+def DIV_ZZ_Z(b_1, n_1, A_1, b_2, n_2, A_2):
+    n_1 = ABS_Z_N(b_1, n_1, A_1)
+    n_2 = ABS_Z_N(b_2, n_2, A_2)
+    # if POZ_Z_D(b_1, n_1, A_1) == 2:
+    #     if POZ_Z_D(b_2, n_2, A_2) == 2:
+
