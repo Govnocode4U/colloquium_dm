7f673f0e3eaf42800a379ae35b7caebdb1362a21
Gnalog13 <102330403+Gnalog13@users.noreply.github.com>
2022-04-06 01:17:18 +0300

    Update N-1.py

--- N-1.py
+++ N-1.py
@@ -25,7 +25,7 @@ def COM_NN_D(n1, arr1, n2, arr2):
         return 1
     else:
         for i in range(0, n1):
-            if (arr1[i] > arr2[2]):
+            if (arr1[i] > arr2[i]):
                 return 2
             elif (arr1[i] < arr2[i]):
                 return 1
