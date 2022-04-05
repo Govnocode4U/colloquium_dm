17ba2a657f914858f519391dd0706a77eeeb34f4
WedsonWestar <wedsonwestar@gmail.com>
2022-04-06 02:04:49 +0300

    Переименовал файл, больше ничего

---
+++ N-2.py
@@ -0,0 +1,5 @@
+def NZER_N_B(n: int, A: list) -> bool:
+    for num in A:
+        if num != 0:
+            return True
+    return False
--- NZER_N_B.py
+++
@@ -1,5 +0,0 @@
-def NZER_N_B(n: int, A: list) -> bool:
-    for num in A:
-        if num != 0:
-            return True
-    return False
