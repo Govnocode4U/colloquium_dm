fb7c55b251228636f058ee2d6ad0e3b3b0bdb62e
Moonlight <44603620+78Moonlight78@users.noreply.github.com>
2022-04-07 02:35:26 +0700

    Delete P5.py

--- P5.py
+++
@@ -1,5 +0,0 @@
-polynom = {1:5, 2:78, 4:128}
-def older_polynomial_coefficient(polynom):
-    coefficient = max(*dict.keys(polynom))
-    return polynom[coefficient]
-print(older_polynomial_coefficient(polynom))
